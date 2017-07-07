39e00892dc951be734277f1640a2082a909de3e1
adaLimana <adairjlimana@aluno.santoangelo.uri.br>
2017-07-07 03:42:36 -0300

    Adaptacao do main apos as alteracoes nas outras classes

--- src/Sinaleira/testeSinaleira.java
+++ src/Sinaleira/testeSinaleira.java
@@ -1,38 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Sinaleira;
 
 import java.util.Scanner;
-/**
- *
- * @author imortal77
- */
+
 public class testeSinaleira {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-    
-        Sinaleira s = new Sinaleira();
+        String opcao = "0";
         Scanner input = new Scanner(System.in);
-        double valor;
-        int i = 0;
-        while(i<10){
-            
-            System.out.println("Informe a quantidade de veiculos");
-            valor = input.nextDouble();
-            s.setQntVeiculos(valor);
-            System.out.println("Informe a quantidade de tempo");
-            valor = input.nextDouble();
-            s.setTempo(valor);
-            s.determinaPrioridade();
-            
-            System.out.printf("%s",s);
-            i++;
+        Cruzamento c = new Cruzamento();
+
+        while(!opcao.equals("q")){
+
+            c.definePrioridadesSinaleiras();
+            c.mostraDadosSinaleiras();
+            c.atualizaDadosSinaleiras();
+            System.out.println("Digite qualquer valor para continuar, ou Q para sair");
+            opcao = input.nextLine();
+            System.out.println(opcao);
         }
     }
     
